7cafd7d32c0c2de6ab7bdb7d894b23f8e7492d58
MAYANKSTUDYSPARK <vermamayank83350s@gmail.com>
2025-10-06 19:48:26 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-api_id = "enter your api id here"
-api_hash = "enter your api hash here"
-bot_token = "enter your bot token here"
-auth_users = [enter your auth user id here]
+api_id = "22657422"
+api_hash = "354881b0718d6f5439d8a523019ffd2e"
+bot_token = "8302978455:AAGfRptubKnYf3MXGpqdZiJTE52i2mYsfjc"
+auth_users = [8219171468]
